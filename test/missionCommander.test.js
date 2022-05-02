5de3117b7374c4c2b1c57ef2a71d359adb21d1d0
Gerardo Juarez Salmoran <gerardo_juarez@hotmail.com>
2022-05-02 17:29:44 -0500

    Se sustituyo el 10 por el 3

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -3,7 +3,7 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
       const result = 1 + 2 
-      expect(result).toBe(10);
+      expect(result).toBe(3);
     });
   })
   
