24928712654dcf43877b59c6efde1bafee71ed63
Gerardo Juarez Salmoran <gerardo_juarez@hotmail.com>
2022-05-02 19:18:06 -0500

    Solucionado algunos problemas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Importing the MissionCommander class from the missionCommander.js file. */
 const MissionCommander = require('./app/missionCommander');
+/* Creating a new instance of the MissionCommander class and assigning it to the variable carlo. */
 const carlo = new MissionCommander("Carlo")
 console.log(carlo.name)
--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,9 +1,13 @@
+/* Importing the MissionCommander class from the missionCommander.js file. */
 const MissionCommander = require('./../app/missionCommander');
 const mymissionCommander = new MissionCommander("Woopa")
 describe("Unit Tests for Mission Commander Class", () => {
     test ('1) Create a mission commander objet', () => {
+        /* Checking to see if the name of the mission commander is "Woopa". */
         expect(mymissionCommander.name).toBe("Woopa");
+      /* Creating a variable called result and assigning it the value of 1 + 2. */
       const result = 1 + 2 
+      /* A test to see if the result of 1 + 2 is 3. */
       expect(result).toBe(3);
     });
   })
